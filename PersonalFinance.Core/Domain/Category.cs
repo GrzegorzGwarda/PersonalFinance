4d83cd9eeaa1c5e267e2d50f657efabb82c6182a
UNILODZ\grzegorz.gwarda <gwarda.grzegorz@gmail.com>
2018-08-20 04:25:37 +0200

    first commit

---
+++ PersonalFinance.Core/Domain/Category.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalFinance.Core.Domain
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public int UserId { get; set; }
+        public virtual User User { get; set; }
+
+        public Category(){}
+
+        public Category(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ PersonalFinance.Core/Domain/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalFinance.Core.Domain
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        public User(){}
+
+        public User(string fullName, string email, string password)
+        {
+            FullName = fullName;
+            Email = email;
+            Password = password;
+            CreatedAt = DateTime.UtcNow;
+        }
+
+
+    }
+}
