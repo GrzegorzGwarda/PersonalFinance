1ac7fff8986f1d2c4fbfe0a075ac11c37fd4f717
UNILODZ\grzegorz.gwarda <gwarda.grzegorz@gmail.com>
2018-09-03 12:04:20 +0200

    Add Autofac Nuget

---
+++ PersonalFinance.Infrastructure/IoC/Modules/RepositoryModule.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalFinance.Infrastructure.IoC.Modules
+{
+    public class RepositoryModule
+    {
+    }
+}
